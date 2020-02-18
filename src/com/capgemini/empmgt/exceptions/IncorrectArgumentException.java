ecb366085c4dc895f3f55ac34498f8ed3084de31
Jyotsna-1234 <sonijyotsna@gmail.com>
2020-02-18 10:56:09 +0530

    first commit

---
+++ src/com/capgemini/empmgt/exceptions/IncorrectArgumentException.java
@@ -0,0 +1,9 @@
+package com.capgemini.empmgt.exceptions;
+
+public class IncorrectArgumentException extends RuntimeException{
+	public IncorrectArgumentException(String msg) {
+        super(msg);
+    }
+
+
+}
---
+++ src/com/capgemini/empmgt/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+
+package com.capgemini.empmgt.service;
+import com.capgemini.empmgt.exceptions.*; 
+
+import com.capgemini.empmgt.dao.IEmployeeDao;
+import com.capgemini.empmgt.entities.Employee;
+import com.capgemini.empmgt.exceptions.IncorrectIdException;
+
+import java.util.Set;
+
+public class EmployeeServiceImpl implements IEmployeeService {
+    private IEmployeeDao dao;
+
+    public EmployeeServiceImpl(IEmployeeDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void addEmployee(Employee e) {
+    	if(e==null)
+    	{
+    		throw new  IncorrectArgumentException("empl is null");
+    	}
+        dao.addEmployee(e);
+    }
+
+    @Override
+    public Employee findEmployeeById(int id) {
+        if (id < 0) {
+            throw new IncorrectIdException("id is incorrect");
+        }
+        Employee e = dao.findEmployeeById(id);
+        return e;
+    }
+
+    @Override
+    public Set<Employee> allEmployees() {
+        Set<Employee> employees = dao.allEmployees();
+        return employees;
+    }
+}
